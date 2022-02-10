875361e78aff18fd4bdbee6bdd02ba18bb0f11eb
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2022-02-10 19:49:21 +0530

    Update bans.py

--- D3VILBOT/modules/bans.py
+++ D3VILBOT/modules/bans.py
@@ -428,7 +428,7 @@ dispatcher.add_handler(UNBAN_HANDLER)
 dispatcher.add_handler(ROAR_HANDLER)
 dispatcher.add_handler(PUNCHME_HANDLER)
 
-__mod_name__ = "BAN/MUTE📍"
+__mod_name__ = "BAN🚫"
 __handlers__ = [
     BAN_HANDLER,
     TEMPBAN_HANDLER,
